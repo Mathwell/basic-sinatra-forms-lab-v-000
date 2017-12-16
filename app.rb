254b9da248f9a3c17e83a6a7ea5ab9275f3e78a5
Olena Ageyeva <eageeva@hotmail.com>
2017-12-16 15:30:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
   get "/newteam" do
-      @user_name = params[:name]
-      "Hello #{@user_name}!"
+      #@user_name = params[:name]
+      #{}"Hello #{@user_name}!"
+      erb: newteam
     end
 
 end
